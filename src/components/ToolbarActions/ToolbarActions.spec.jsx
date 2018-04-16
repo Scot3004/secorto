e46245374ec5a5038042c0cdde8a946698b65812
Sergio C. Orozco Torres <scot3004@gmail.com>
2018-04-16 12:34:58 -0500

    Test ToolbarActions

---
+++ src/components/ToolbarActions/ToolbarActions.spec.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import ToolbarActions from './ToolbarActions';
+
+const config = "stub"
+
+it('renders correctly', () => {
+  const tree = shallow(<ToolbarActions config={config} />);
+  expect(toJson(tree)).toMatchSnapshot();
+});
