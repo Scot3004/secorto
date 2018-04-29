b430856f31d020b8ffe4afc2ac345645284c29ae
Sergio C. Orozco Torres <scot3004@gmail.com>
2018-04-28 19:11:19 -0500

    Gastby-config test

---
+++ __tests__/gatsby-config.spec.js
@@ -0,0 +1,45 @@
+import gatsbyConfig from '../gatsby-config';
+
+
+describe('Gastby config', () => {
+  it('should be equals to snapshot', () => {
+    expect(gatsbyConfig).toMatchSnapshot();
+  });
+});
+
+describe("Setup for gatsby-plugin-feed", () => {
+  const ref = {
+    query: {
+      allMarkdownRemark: {
+        edges:[
+          {
+            node: {
+              frontmatter: {
+                tags: ["prueba"],
+                date: "date",
+                title: "title",
+                excerpt: "excerpt"
+              },
+              fields: {
+                slug: "slug"
+              },
+              html: "html"
+            }
+          }
+        ],
+      },
+      site: {
+        siteMetadata: {
+          rssMetadata: {
+            siteUrl: "siteUrl/"
+          }
+        }
+      }
+    }
+  }
+  const feedSetup = gatsbyConfig.plugins.find((element) => (element.resolve === "gatsby-plugin-feed"));
+  it('Snapshot of Setup for gatsby-plugin-feed', () => {
+    expect(feedSetup.options.setup(ref)).toMatchSnapshot()
+  });
+})
+
